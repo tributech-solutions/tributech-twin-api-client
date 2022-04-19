afc01ff388bab26e3c2ca94c5fe5ef0f7b9731dc
dhofer <d.hofer@tributech.io>
2022-04-19 08:41:58 +0200

    feat: added config class and functions to easily create client

---
+++ clients/netcore/TwinApiClientFactory.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Net.Http;
+
+namespace Tributech.Dsk.TwinApi.Client;
+
+public static class TwinApiClientFactory
+{
+    public static TwinApiClient FromHttpClient(HttpClient httpClient)
+    {
+        return new TwinApiClient(httpClient);
+    }
+
+    public static TwinApiClient FromOptions(TwinApiClientOptions options)
+    {
+        var socketsHttpHandler = new SocketsHttpHandler
+        {
+            PooledConnectionLifetime = TimeSpan.FromMinutes(5)
+        };
+        var authHandler = new APIAuthHandler(options.TokenUrl, string.Join(' ', options.Scopes), options.ClientId, options.ClientSecret, socketsHttpHandler);
+        var httpClient = new HttpClient(authHandler)
+        {
+            BaseAddress = new Uri(options.ApiEndpoint),
+            Timeout = TimeSpan.FromSeconds(30)
+        };
+
+        return FromHttpClient(httpClient);
+    }
+
+    public static TwinApiClient FromConfiguration(IConfiguration config, string configSectionName = nameof(TwinApiClientOptions))
+    {
+        TwinApiClientOptions options = new();
+        config.Bind(configSectionName, options);
+
+        return FromOptions(options);
+    }
+}
